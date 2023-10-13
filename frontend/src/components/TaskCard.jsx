cf18896d269334c47f7ea9ccfbc27ceb5d9bd918
Joseph Anthony Debono <joe@jadebono.com>
2023-10-13 18:54:47 +0200

    added color coding and priority sorting

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -8,10 +8,20 @@ const TaskCard = ({ task, onDelete }) => {
   return (
     <div className="m-4 p-4 bg-gray-100 rounded-lg shadow-md w-full max-w-md border border-blue-900 bg-blue-50">
       {/* Header Bar */}
-      <div className="bg-blue-500 text-white p-2 rounded-lg flex justify-between items-center">
+      <div
+        className={`text-white p-2 rounded-lg flex justify-between items-center ${
+          task.priority === "high"
+            ? "bg-red-700"
+            : task.priority === "medium"
+            ? "bg-orange-700"
+            : task.priority === "low"
+            ? "bg-green-700"
+            : "bg-blue-500"
+        }`}
+      >
         <h2 className="font-bold">Task</h2>
         <button onClick={() => onDelete(task.id)}>
-          <p className="text-white font-extrabold bg-red-400 hover:bg-red-700 p-1 px-2 rounded-lg">
+          <p className="text-black font-extrabold bg-white hover:bg-grey-200 p-1 px-2 rounded-lg">
             x
           </p>
         </button>
@@ -20,7 +30,6 @@ const TaskCard = ({ task, onDelete }) => {
       {/* Task Details */}
       <div className="mt-4">
         {" "}
-        {/* Added margin-top for spacing */}
         <div>
           <p className="mb-2 text-blue-900 inline-block">Task:&nbsp;</p>
           <p className="inline-block font-extrabold text-indigo-900">
@@ -35,7 +44,17 @@ const TaskCard = ({ task, onDelete }) => {
         </div>
         <div>
           <p className="mb-2 text-blue-900 inline-block">Priority:&nbsp;</p>
-          <p className="inline-block font-extrabold text-indigo-900">
+          <p
+            className={`inline-block font-extrabold ${
+              task.priority === "high"
+                ? "text-red-700"
+                : task.priority === "medium"
+                ? "text-orange-700"
+                : task.priority === "low"
+                ? "text-green-700"
+                : "text-indigo-900"
+            }`}
+          >
             {task.priority}
           </p>
         </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,8 +6,27 @@ import TaskCard from "../components/TaskCard";
 import { deleteTask } from "../store/TaskSlice";
 
 export default function Home() {
-  const tasks = useSelector((state) => state.tasks.list); // Get tasks from Redux store
+  const tasks = useSelector((state) => state.tasks.list);
   const dispatch = useDispatch();
+
+  // Assign a numerical value for each priority level for easy sorting
+  const priorityValue = (priority) => {
+    switch (priority) {
+      case "high":
+        return 1;
+      case "medium":
+        return 2;
+      case "low":
+        return 3;
+      default:
+        return 4;
+    }
+  };
+
+  // Sort the tasks by priority
+  const sortedTasks = [...tasks].sort(
+    (a, b) => priorityValue(a.priority) - priorityValue(b.priority)
+  );
 
   // Delete function
   const handleDelete = (id) => {
@@ -19,7 +38,7 @@ export default function Home() {
       <div>
         <TaskCreationArea />
         <div className="flex flex-wrap justify-start">
-          {tasks.map((task) => (
+          {sortedTasks.map((task) => (
             <TaskCard key={task.id} task={task} onDelete={handleDelete} />
           ))}
         </div>
